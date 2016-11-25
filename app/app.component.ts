769092a849238fa81730ddb277cedf15ee78a4b7
mensahGit <mensah@m.local>
2016-11-25 14:07:33 -0300

    Third Commit

---
+++ app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+	selector: 'my-app',
+	template: 
+	`
+	<h1>Mensah\'s Angular2 Scaffolding</h1>
+	`,
+  styleUrls: ['styles.css'],
+})
+
+
+export class AppComponent {}
---
+++ app/app.module.ts
@@ -0,0 +1,14 @@
+//MODULES//
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+//COMPONENTS//
+import { AppComponent } from './app.component';
+
+@NgModule({
+    imports: [ BrowserModule ],
+    declarations: [ AppComponent ],
+    bootstrap: [ AppComponent ]
+    
+})
+
+export class AppModule {}
